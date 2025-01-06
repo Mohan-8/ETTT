5ba04d9cff6f919132d50bbce8f6606afecc28f9
Mohan-8 <smohanakrishnan82@gmail.com>
2025-01-06 20:46:41 +0530

    up

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
     <DAppProvider config={{}}>
       <Router>
         <Routes>
-          <Route path="/port" element={<App />} />
+          <Route path="/" element={<App />} />
         </Routes>
       </Router>
     </DAppProvider>
